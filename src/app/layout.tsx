a3a429a31f23c059feba0a7aaa608c37c1b56124
Ankamo <102838840+Ankamo@users.noreply.github.com>
2024-11-04 01:03:40 +0000

    Create App Vr. 1.0.0.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 // src/app/layout.tsx
 import React from 'react';
 import '../styles/globals.css'; // Importa los estilos globales
-import Header from '../components/Header'; // Cambia a HeaderNavbar
-import Footer from '../components/Footer';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <html lang="es">
             <body>
-                <Header /> {/* Cambia esto también */}
                 <main>{children}</main>
-                <Footer />
             </body>
         </html>
     );
